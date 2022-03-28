706eca7bbc055e1331e21ea7a4a5af61952de52d
Fayed-Rsl <84181583+Fayed-Rsl@users.noreply.github.com>
2022-03-28 07:47:05 +0200

    Database SQL
    database is not txt but sql lite now
    next: add txt input for push up, pull up ...

--- database.py
+++ database.py
@@ -1,51 +1,180 @@
-import datetime
+from datetime import date
+import sqlite3
+
+from sqlite3 import Error as SqlError
+
+class Database:
+    def __init__(self, path):
+        '''Connect to the database'''
+        self.conn = self.connection(path)
+        self.cursor = self.conn.cursor()
+
+    def connection(self, path):
+        '''connect to SQL lite database'''
+        connection = None
+        try:
+            connection = sqlite3.connect(path)
+            print("Connection to SQLite DB successful")
+            return connection
+        except SqlError as e:
+            print(f"Error ID: Connection. The error '{e}' occurred")
+
+    def drop_table(self, table:str):
+        '''name of the table
+        table:str
+        '''
+        try:
+            self.cursor.execute("DROP TABLE IF EXISTS" + ' ' + table)
+            self.conn.commit()
+            print(f'Dropped table {table}' )
+        except SqlError as e:
+            print(f"Error ID: Drop Table. The error '{e}' occurred")
+
+    def create_user_table(self):
+        '''create user table
+        username:str (unique)
+        password:str
+        email:str (unique)
+        timestamp:date'''
+        try:
+            self.conn.execute('''CREATE TABLE IF NOT EXISTS _User
+                    (username CHAR(20) NOT NULL UNIQUE,
+                    password CHAR(50) NOT NULL,
+                    email CHAR(255) NOT NULL UNIQUE,
+                    timestamp DATE NOT NULL)''')
+            self.conn.commit()
+            print(f'User Table successfully created.')
+        except SqlError as e:
+            print(f"Error ID: Create _User. The error '{e}' occurred")
 
 
-class DataBase:
-    def __init__(self, filename):
-        self.filename = filename
-        self.users = None
-        self.file = None
-        self.load()
+    def create_user_goal(self):
+        '''create user goal table
+        user_id:str (unique)
+        goal:int '''
+        try:
+            self.conn.execute('''CREATE TABLE IF NOT EXISTS _UserGoal
+                    (user_id INT NOT NULL PRIMARY KEY UNIQUE,
+                    push_up INT NOT NULL,
+                    pull_up INT NOT NULL,
+                    abdo INT NOT NULL,
+                    squat INT NOT NULL)''')
+            self.conn.commit()
+            print(f'UserGoal Table successfully created.')
+        except SqlError as e:
+            print(f"Error ID: Create _UserGoal. The error '{e}' occurred")
 
-    def load(self):
-        self.file = open(self.filename, "r")
-        self.users = {}
+    def create_userdata_table(self):
+        '''create userdata table that will store every data of users
+        user_id:rowid of user
+        push_up:int
+        pull_up:int
+        abdo:int
+        day:date'''
+        try:
+            self.conn.execute('''CREATE TABLE IF NOT EXISTS _UserData
+                    (user_id INT NOT NULL,
+                    push_up INT NOT NULL,
+                    pull_up INT NOT NULL,
+                    abdo INT NOT NULL,
+                    squat INT NOT NULL,
+                    day DATE NOT NULL,
+                    PRIMARY KEY (USER_ID, DAY) )''') # composite key: USER_ID + DAY
+            self.conn.commit()
+            print(f'User DataTable successfully created.')
 
-        for line in self.file:
-            email, password, name, created = line.strip().split(";")
-            self.users[email] = (password, name, created)
+        except SqlError as e:
+            print(f"Error ID: Create _UserData. The error '{e}' occurred")
 
-        self.file.close()
+    def insert_user(self, value:tuple):
+        '''insert new user in the _User table
+        value:tuple (username, password, email, timestamp)'''
+        try:
+            self.cursor.execute('''INSERT INTO _User (username, password, email, timestamp)
+            VALUES (?, ?, ?, ?)''', value)
+            self.conn.commit()
+            print(f'User {value} successfully inserted in Database.')
+            return self.cursor.lastrowid
+        except SqlError as e:
+            print(f"Error ID: Insert User. The error '{e}' occurred")
 
-    def get_user(self, email):
-        if email in self.users:
-            return self.users[email]
-        else:
-            return -1
+    def insert_goal(self, value:tuple):
+        '''insert goal user in the _UserGoal table
+        value:tuple (user_id, goal)'''
+        try:
+            self.cursor.execute('''INSERT INTO _UserGoal (user_id, goal)
+            VALUES (?, ?)''', value)
+            self.conn.commit()
+            print(f'UserGoal {value} successfully inserted in Database.')
+        except SqlError as e:
+            print(f"Error ID: Insert Goal. The error '{e}' occurred")
 
-    def add_user(self, email, password, name):
-        if email.strip() not in self.users:
-            self.users[email.strip()] = (password.strip(), name.strip(), DataBase.get_date())
-            self.save()
-            return 1
-        else:
-            print("Email exists already")
-            return -1
+    def insert_data(self, value:tuple):
+        '''insert new userdata in the _UserData table
+        value:tuple (user_id, push_up, pull_up, abdo, squat, day)'''
 
-    def validate(self, email, password):
-        if self.get_user(email) != -1:
-            return self.users[email][0] == password
-        else:
-            return False
+        try:
+            self.cursor.execute('''INSERT INTO _UserData (user_id, push_up, pull_up, abdo, squat, day)
+            VALUES (?, ?, ?, ?, ?, ?)''', value)
+            self.conn.commit()
+            print(f'UserData successfully inserted in Database.')
+        except SqlError as e:
+            print(f"Error ID: Insert Data. The error '{e}' occurred")
+            
+    def update_data(self, value:tuple):
+        '''update userdata in the _UserData table
+        value:tuple (push_up, pull_up, abdo, squat, user_id, day)'''
 
-    def save(self):
-        with open(self.filename, "w") as f:
-            for user in self.users:
-                f.write(user + ";" + self.users[user][0] + ";" + self.users[user][1] + ";" + self.users[user][2] + "\n")
+        try:
+            self.cursor.execute('''UPDATE _UserData
+            SET push_up = ?, pull_up = ?, abdo = ?, squat = ?
+            WHERE user_id = ? AND day = ?''', value)
+            self.conn.commit()
+            print(f'UserData {value} successfully updated in Database.')
+        except SqlError as e:
+            print(f"Error ID: Update Data. The error '{e}' occurred")
+
+    def get_user_id(self, value:tuple):
+        '''get the user id
+        value:tuple (username, email)
+        return: integer (user rowid)'''
+        try:
+            self.cursor.execute('''SELECT rowid FROM _User WHERE email = ? ''', value)
+            USER_ID = self.cursor.fetchone()[0]
+            print(f'User rowid: {USER_ID}')
+            return USER_ID
+        except SqlError as e:
+            print(f"Error ID: Get UserID. The error '{e}' occurred")
+
+    def check_primary_key(self, value:tuple):
+        '''check if user already entered data today.
+        value:tuple (user_id, day)
+        return 1 or 0'''
+        try:
+            self.cursor.execute('''SELECT user_id, day FROM _UserData WHERE user_id = ? AND day = ? ''', value)
+            CHECK = self.cursor.fetchone()
+
+            # if user didnt entered data today return 1
+            if CHECK != None: return 1
+            else: return 0 
+        except SqlError as e:
+            print(f"Error ID: Check PK. The error '{e}' occurred")        
+
+    def check_account(self, value:tuple):
+        '''check if the account exist and if correct password
+        value:tuple (email, password)
+        return 1 or 0'''
+        try:
+            self.cursor.execute('''SELECT email, password FROM _User WHERE email = ? AND password = ? ''', value)
+            CHECK = self.cursor.fetchone()
+
+            # if email and password does not exist return 1
+            if CHECK != None: return 1
+            else: return 0 
+        except SqlError as e:
+            print(f"Error ID: Check Account. The error '{e}' occurred")        
 
     @staticmethod
     def get_date():
-        return str(datetime.datetime.now()).split(" ")[0]
-
-
+        '''function to get today date'''
+        return date.today().strftime('%Y-%m-%d')
--- main.py
+++ main.py
@@ -1,24 +1,29 @@
+from sqlite3 import DatabaseError
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.properties import ObjectProperty
 from kivy.uix.popup import Popup
 from kivy.uix.label import Label
-from database import DataBase
+from database import Database
 
 
 class CreateAccountWindow(Screen):
-    namee = ObjectProperty(None)
-    email = ObjectProperty(None)
+    username = ObjectProperty(None)
     password = ObjectProperty(None)
+    email = ObjectProperty(None)
+    user_id = None
 
     def submit(self):
-        if self.namee.text != "" and self.email.text != "" and self.email.text.count("@") == 1 and self.email.text.count(".") > 0:
-            if self.password != "":
-                db.add_user(self.email.text, self.password.text, self.namee.text)
+        global user_id
+        '''function to create an account'''
+        if (self.username.text != '' and self.email.text != '' and
+            self.email.text.count('@') == 1 and self.email.text.count('.') > 0):
+            if self.password != '':
+                user = (self.username.text, self.password.text, self.email.text, today)
+                user_id = db.insert_user( user )
 
                 self.reset()
-
                 sm.current = "login"
             else:
                 invalidForm()
@@ -30,18 +35,19 @@ class CreateAccountWindow(Screen):
         sm.current = "login"
 
     def reset(self):
-        self.email.text = ""
+        self.username.text = ""
         self.password.text = ""
-        self.namee.text = ""
-
+        self.email.text = ""
 
 class LoginWindow(Screen):
     email = ObjectProperty(None)
     password = ObjectProperty(None)
-
+    user_id = None
     def loginBtn(self):
-        if db.validate(self.email.text, self.password.text):
+        global user_id
+        if db.check_account( (self.email.text, self.password.text)) == 1:
             MainWindow.current = self.email.text
+            user_id = db.get_user_id( (self.email.text,) )
             self.reset()
             sm.current = "main"
         else:
@@ -57,20 +63,34 @@ class LoginWindow(Screen):
 
 
 class MainWindow(Screen):
-    n = ObjectProperty(None)
-    created = ObjectProperty(None)
-    email = ObjectProperty(None)
-    current = ""
+    '''Main menu window to:
+    enter goal - select type of sport'''
+    goal = ObjectProperty(None)
+
+    # add des widget pr chaque type d'action
 
     def logOut(self):
         sm.current = "login"
+    
+    def goalBtn(self):
+        if self.goal.text != '':
+            print(self.goal.text, type(self.goal.text))
+            db.insert_goal( (user_id, self.goal.text,) )
+            self.reset()
+            # sm.current = "" # next page 
+    
+    def reset(self):
+        self.goal.text = ""
 
-    def on_enter(self, *args):
-        password, name, created = db.get_user(self.current)
-        self.n.text = "Account Name: " + name
-        self.email.text = "Email: " + self.current
-        self.created.text = "Created On: " + created
-
+    # def on_enter(self, *args):
+    #     pass
+        # if db.check_primary_key( (user_id, today)) == 1:
+        #     data = ('', '')
+        #     db.update_data( (200, 340, 500, 140, user_id, today) )
+        # else:
+        #     data = ('', '')
+        #     db.insert_data( (user_id, 300, 600, 800, 40, today) )
+        #                 self.reset()
 
 class WindowManager(ScreenManager):
     pass
@@ -91,17 +111,24 @@ def invalidForm():
     pop.open()
 
 
-kv = Builder.load_file("my.kv")
+kv = Builder.load_file("my.kv") # load the kv file
 
-sm = WindowManager()
-db = DataBase("users.txt")
+sm = WindowManager() # screen manager 
+db = Database ('database.db') # sql lite database
+today = db.get_date()
+# db.drop_table('_User')
+# db.drop_table('_UserData')
+db.drop_table('_UserGoal')
+# db.create_user_table()
+# db.create_userdata_table()
+db.create_user_goal()
 
+# every screen on our application
 screens = [LoginWindow(name="login"), CreateAccountWindow(name="create"),MainWindow(name="main")]
 for screen in screens:
-    sm.add_widget(screen)
+    sm.add_widget(screen) # add every screen in the screen manager
 
-sm.current = "login"
-
+sm.current = "login" # the first page to show = login
 
 class MyMainApp(App):
     def build(self):
